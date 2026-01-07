f1d586f941349ac99f21d0d2f69ad799b09945ff
shanthoshmobileshub <shanthoshmobileshub@gmail.com>
2026-01-07 15:20:47 +0530

    First upload of project

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // Set `base` to the repository name so GH Pages serves assets correctly
 export default defineConfig({
-  base: '/MySite/',
+  base: '/SMH2026/',
   plugins: [react()]
 })
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000
-  }
-})
