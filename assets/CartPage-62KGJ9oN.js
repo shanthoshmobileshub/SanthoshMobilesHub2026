3ff375fc40837d15faa05a031a391622e5690557
shanthoshmobileshub <shanthoshmobileshub@gmail.com>
2026-01-18 01:04:54 +0530

    Updates

--- assets/CartPage-62KGJ9oN.js
+++ assets/CartPage-62KGJ9oN.js
@@ -1,4 +1,4 @@
-import{a as m,u as p,j as e,g as u,h as i,i as b,A as y}from"./index-CHjnFOCb.js";import{F as j}from"./Footer-B_uQhVLD.js";function v(){const{cart:a=[],removeFromCart:o,clearCart:c}=m(),s=p(),l=t=>{if(typeof t=="number")return t;if(!t)return 0;const r=String(t).replace(/,/g,"");return Number(r)||0},n=a.reduce((t,r)=>t+l(r.price)*(r.qty||1),0),x=()=>{let t=`👋 Hello Santhosh Mobiles, I would like to place an order:
+import{a as m,u as p,j as e,g as u,h as i,k as b,A as y}from"./index-boQFZw8u.js";import{F as j}from"./Footer-DpCSg6jQ.js";function v(){const{cart:a=[],removeFromCart:o,clearCart:c}=m(),s=p(),l=t=>{if(typeof t=="number")return t;if(!t)return 0;const r=String(t).replace(/,/g,"");return Number(r)||0},n=a.reduce((t,r)=>t+l(r.price)*(r.qty||1),0),x=()=>{let t=`👋 Hello Santhosh Mobiles, I would like to place an order:
 
 `;a.forEach((d,h)=>{const g=l(d.price);t+=`${h+1}. *${d.title}* 
    Price: ₹${g.toLocaleString("en-IN")} x ${d.qty||1}
