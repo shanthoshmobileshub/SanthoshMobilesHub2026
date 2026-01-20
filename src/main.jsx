4f11aae97e6de7799c9d8dc7f9b0f087f5eccfac
shanthoshmobileshub <shanthoshmobileshub@gmail.com>
2026-01-21 00:53:13 +0530

    Force Update logic and Deploy

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
+
+console.log("App Version: 2026-01-21 Force Update");
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename={import.meta.env.BASE_URL}>
